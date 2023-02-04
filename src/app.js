038a481eeea07a13232c2d5ac00be1beada2d72b
ronnychiang <ronnychiang1164@gmail.com>
2023-02-04 22:45:08 +0800

    feat: get medium stats

---
+++ src/app.js
@@ -0,0 +1,77 @@
+const axios = require('axios');
+const { GistBox } = require('gist-box');
+const cheerio = require('cheerio');
+const table = require('text-table');
+
+require('dotenv').config();
+
+const MEDIUM_API_BASE_URL =
+  'https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@';
+
+const { GIST_ID, GH_PAT, MEDIUM_USER_NAME } = process.env;
+const MAX_STR_LENGTH = 20;
+
+if (!GIST_ID || !GH_PAT || !MEDIUM_USER_NAME)
+  throw new Error('Get data failed');
+
+async function getMediumStats() {
+  let takenData = '';
+  let result = [];
+  try {
+    const mediumRes = await axios.get(MEDIUM_API_BASE_URL + MEDIUM_USER_NAME);
+
+    takenData = await Promise.all(
+      mediumRes.data.items
+        .slice(1, 4) // 3 stories
+        .map(async (item) => {
+          const res = await axios.get(item.guid); // story url
+          const $ = cheerio.load(res.data);
+          const commentCount = $('.pw-responses-count').text();
+          return {
+            title: item.title,
+            date: item.pubDate,
+            comments: commentCount ? commentCount : '0',
+          };
+        })
+    );
+  } catch (err) {
+    throw new Error('catch data failed');
+  }
+
+  takenData.forEach((item) => {
+    const day = new Date(item.date); // set date form
+
+    // set title length
+    let trimTitle = '';
+    if (item.title.length > MAX_STR_LENGTH)
+      trimTitle = item.title.slice(0, MAX_STR_LENGTH) + '...';
+    else trimTitle = item.title;
+
+    result.push([
+      trimTitle,
+      day.toLocaleDateString(),
+      `comments:${item.comments}`,
+    ]);
+  });
+
+  result = table(
+    [[`Medium @${MEDIUM_USER_NAME}`], ['Latest Stories:'], ...result],
+    {
+      align: ['l', 'r'],
+      stringLength: () => 20,
+    }
+  );
+
+  const box = new GistBox({ id: GIST_ID, token: GH_PAT });
+
+  try {
+    await box.update({
+      filename: 'medium-stat.md',
+      content: result,
+    });
+  } catch (err) {
+    throw new Error('push to gist failed');
+  }
+}
+
+getMediumStats();
